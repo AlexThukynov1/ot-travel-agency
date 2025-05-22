0e09401e3ce86e9bd3bf7df615cf520cccd888ac
AlexThukynov1 <alexthukynov@gmail.com>
2025-05-22 12:02:24 +0300

    add section title component

--- src/sections/Categories/index.jsx
+++ src/sections/Categories/index.jsx
@@ -1,8 +1,26 @@
+import { RiArrowLeftDoubleFill, RiArrowRightDoubleFill } from 'react-icons/ri'
+import SectionTitle from '../../components/SectionTitle'
 import './categories.css'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <section className="category section">
+      <div className="category-header">
+        <SectionTitle 
+          title="Categories"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+
+        <div className="swiper-btns">
+          <button className='swipper-btn prev-btn'>
+            <RiArrowLeftDoubleFill/>
+          </button>
+          <button className='swipper-btn next-btn'>
+            <RiArrowRightDoubleFill/>
+          </button>
+        </div>
+      </div>
+    </section>
   ) 
 }
 
