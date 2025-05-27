ac61e83fcddb8323d747188d004ba66813bbc3b8
AlexThukynov1 <alexthukynov@gmail.com>
2025-05-27 13:56:38 +0300

    start tours section

--- src/sections/Tours/index.jsx
+++ src/sections/Tours/index.jsx
@@ -1,8 +1,25 @@
 import './tours.css'
+import SectionTitle from "../../components/SectionTitle/index.jsx";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import TourCard from "../../components/TourCard/index.jsx";
+import {tours} from "../../data/Data.jsx";
+
+
 
 const Tours = () => {
   return (
-    <div>Tours</div>
+      <section className="tours container section">
+        <div className="tours-header">
+          <SectionTitle
+            title="Select Your Tour"
+            subtitle="Epic Adventures Tour"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+        </div>
+      </section>
   ) 
 }
 
